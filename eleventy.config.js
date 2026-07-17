7e0abc5e905099156a3513cd6881d6327e4758ec
Dillon Morse <dillon22morse@gmail.com>
2026-07-17 14:08:57 -0400

    Add Some of Our Deals PDF link under featured listing

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
-  ["404.html", "privacy.html", "robots.txt", "sitemap.xml", "og-image.png", "admin", "uploads", "_redirects"]
+  ["404.html", "privacy.html", "robots.txt", "sitemap.xml", "og-image.png", "some-of-our-deals.pdf", "admin", "uploads", "_redirects"]
     .forEach(p => eleventyConfig.addPassthroughCopy("src/" + p));
   return {
     dir: { input: "src", output: "_site" },
